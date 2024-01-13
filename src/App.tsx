75a472093582c2ff1d0367b5c553f4639fba9c8d
Abu <abujakaria316@gmail.com>
2024-01-14 00:16:34 +0600

    tailwind css config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import MainLayout from "./components/layout/MainLayout";
-
 function App() {
-  return <MainLayout />;
+  return (
+    <div className="flex flex-col mx-auto justify-center items-center">
+      <div>
+        <h1 className="mr-3"> Hello Bangladesh</h1>
+        <h2 className="mr-3">Hello Duniya</h2>
+        <h3>Hello World!</h3>
+      </div>
+      <div>
+        <button className="btn btn-primary">Primary</button>
+
+        <button className="btn btn-danger">Danger</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
